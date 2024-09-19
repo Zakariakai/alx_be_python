9d7ac1430b860fcd9c8c23f7b7d77f941881cdb2
Zakariakai <zakariakaifouf@gmail.com>
2024-09-19 04:03:15 +0100

    Add daily_reminder.py to provide customized task reminders based on priority and time sensitivity

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -10,8 +10,8 @@ match priority:
         reminder = f"Reminder: '{task}' is a low priority task"
     case _:
         reminder = f"Reminder: '{task}' has an unknown priority level"
-
 if time_bound == "yes":
     reminder += "that requires immediate attention today!"
 else:
     reminder += "Consider completing it when you have free time."
+print(reminder)
