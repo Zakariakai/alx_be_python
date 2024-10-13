322f8f6507a512baa354bf4e17a8f017b7516be7
Zakariakai <zakariakaifouf@gmail.com>
2024-10-13 01:50:08 +0100

    Add all files to the directory

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,34 +1,14 @@
 class Book:
-    def __init__(self, title: str, author: str):
+    def __init__(self, title: str, author: str, year: int):
         self.title = title
         self.author = author
+        self.year = year
+
+    def __del__(self):
+        print(f"Deleting {self.title}")
 
     def __str__(self):
-        return f"Book: {self.title} by {self.author}"
+        return f"{self.title} by {self.author}, published in {self.year}"
 
-class EBook(Book):
-    def __init__(self, title: str, author: str, file_size: int):
-        super().__init__(title, author)
-        self.file_size = file_size
-
-    def __str__(self):
-        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
-
-class PrintBook(Book):
-    def __init__(self, title: str, author: str, page_count: int):
-        super().__init__(title, author)
-        self.page_count = page_count
-
-    def __str__(self):
-        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
-
-class Library:
-    def __init__(self):
-        self.books = []
-
-    def add_book(self, book: Book):
-        self.books.append(book)
-
-    def list_books(self):
-        for book in self.books:
-            print(book)
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
